245de3458c0a99f2c03cf299b63c15f6cfd19397
huy1231a <taolahuy2k0@gmail.com>
2024-03-17 22:29:38 +0700

    add layout mes

---
+++ src/Layout/Messenger/Boxchat/index.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import './style.css'
+import { Avatar, Badge, Typography } from 'antd'
+
+interface Prop {
+    nameBox: string
+    messageBox: string
+    time: string
+    numberOfNoti?: number
+    img?: any
+}
+
+const BoxChat: React.FC<Prop> = ({
+    nameBox,
+    messageBox,
+    time,
+    numberOfNoti,
+    img
+}) => {
+    const [show, setShow] = useState(true);
+    const { Text } = Typography
+    return (
+        <div className='box__chat'>
+            <div className='box__chat__name'>
+                <Avatar style={{ width: 40, height: 40 }} src={img} />
+                <div className='box__chat__name__'>
+                    <span className='title'>{nameBox}</span>
+                    <Text type='secondary' className='overflow-ellipsis'>{messageBox}</Text>
+                </div>
+            </div>
+            <div className='box__chat__name__2'>
+                <Text type='secondary'>{time}</Text>
+                <Badge count={numberOfNoti} />
+            </div>
+        </div>
+    )
+}
+
+export default BoxChat
--- src/Layout/Messenger/index.tsx
+++ src/Layout/Messenger/index.tsx
@@ -1,7 +1,103 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './style.css'
+import { Avatar, Button, Divider, Menu, MenuProps, Typography } from 'antd'
+import { svg__search, svg__filter, svg__dot } from './Constans';
+import { MailOutlined, AppstoreOutlined, SearchOutlined, PlusOutlined } from '@ant-design/icons'
+import BoxChat from './Boxchat';
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: 'group',
+  onClick?: any
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+    onClick
+  } as MenuItem;
+}
+
+const items: MenuProps['items'] = [
+  getItem('Groups', 'sub1', <MailOutlined />, [
+    getItem(<><BoxChat messageBox='dit cu chung may may thag ngu' nameBox='Medical App Team' time='12:04' numberOfNoti={24} /></>, '1'),
+    getItem(<><BoxChat messageBox='dit cu chung may may thag ngu' nameBox='Food Delivery Service' time='12:04' numberOfNoti={1} /></>, '2'),
+
+  ]),
+  getItem('Direct Messages', 'sub2', <AppstoreOutlined />, [
+    getItem(<><BoxChat messageBox='dit cu chung may may thag ngu' nameBox='Garrett Watson' time='12:04' /> </>, '5'),
+    getItem(<><BoxChat messageBox='dit cu chung may may thag ngu' nameBox='Caroline Santos' time='12:04' /> </>, '6'),
+  ]),
+
+
+];
 
 const Messenger = () => {
-  return <div style={{ height: '1000px' }}>Messenger</div>
+  const { Title, Text } = Typography
+
+
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    console.log('click ', e);
+  };
+
+  return (
+    <div className='mesenger'>
+      <Title level={2} style={{ fontWeight: 600 }} className='header__mes'>
+        Messenger
+      </Title>
+      <div className='chat__mes'>
+        <div className='left__chat__ct'>
+          <div className='header__chat__ct'>
+            <div className='top__contend'>
+              <Title level={4} style={{ fontWeight: 600, margin: 'auto' }} className='header__mes'>
+                Conversations
+              </Title>
+            </div>
+            <div className='btn__search'>
+              <Button className='box__1'><SearchOutlined /></Button>
+              <Button type='primary' className='box__2'><PlusOutlined /></Button>
+            </div>
+          </div>
+
+        </div>
+        <div className='diveder__chat'></div>
+        <div className='right__chat__ct'>
+          <div className='left__title'>
+            <Avatar style={{ width: 50, height: 50 }} />
+            <div className='left__job__name'>
+              <span className='title'>Oscar Holloway</span>
+              <Text type='secondary'>UI/UX Designer</Text>
+            </div>
+          </div>
+          <div>
+            <div className='box__'>
+              <div className='box__1'>{svg__search}</div>
+              <div className='box__1'>{svg__filter}</div>
+              <div className='box__1'>{svg__dot}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Divider style={{ margin: '0px auto' }} />
+      <div className='botton__chat'>
+        <Menu
+          mode="inline"
+          onClick={onClick}
+          style={{ width: '25%' }}
+          items={items}
+        />
+      </div>
+
+    </div>
+  )
 }
 
 export default Messenger
